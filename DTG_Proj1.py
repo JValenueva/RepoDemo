bd0b46fd1575b291eb05ecf09c094fac282d5a4f
JValenueva <rykerluno@gmail.com>
2023-01-10 18:24:25 -0700

    Code lines 2-7 have been placed in new project folder from day_trip_gen.py

--- DTG_Proj1.py
+++ DTG_Proj1.py
@@ -1,1 +1,7 @@
 print('Test one two, test one two')
+import random
+
+destination_list = ['Georgia', 'Utah', 'California', 'Nevada']
+restaurants_list = ['Bacoa Finca + Fogon','Cafe Mutton',"Smoke'N Ash BBQ", 'San Ho Won']
+transport_means_list = ['car','plane','train']
+entertainment_list = ['Club/Bar','Cinema/Theater','Museum/Tour Bus','Hiking/Mountain Climbing']
